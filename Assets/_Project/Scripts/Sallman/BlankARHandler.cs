ea2f8f6cfd78796f3ae85b7371174f29fc2ddd8e
SallmanRafi <salman.rafi26@gmail.com>
2026-03-09 02:37:34 +0500

    xfxxg

--- Assets/_Project/Scripts/Sallman/BlankARHandler.cs
+++ Assets/_Project/Scripts/Sallman/BlankARHandler.cs
@@ -2,28 +2,12 @@ using UnityEngine;
 
 public class BlankARHandler : MonoBehaviour
 {
-    void Update()
-    {
-        SetTempChildrenToZero(transform);
-    }
-
-    void SetTempChildrenToZero(Transform parent)
-    {
-        foreach (Transform child in parent)
-        {
-            if (child.name.Contains("[TEMP Layer]"))
-            {
-                child.localPosition = Vector3.zero;
-            }
-
-            // Continue searching deeper
-            SetTempChildrenToZero(child);
-        }
-    }
-    
     private Transform workSpace;
     private Transform workLayer;
-    
+
+    private Vector3 startLocalPos;
+    private Quaternion startLocalRot;
+
     void Start()
     {
         Transform[] all = GetComponentsInChildren<Transform>(true);
@@ -36,36 +20,82 @@ public class BlankARHandler : MonoBehaviour
             if (t.name.Contains("Work Layer"))
                 workLayer = t;
         }
+
+        if (workLayer != null)
+        {
+            startLocalPos = workLayer.localPosition;
+
+            // force Z to negative
+            startLocalPos.z = -Mathf.Abs(startLocalPos.z);
+
+            startLocalRot = workLayer.localRotation;
+        }
     }
 
-    void OnGUI()
+    void Update()
     {
-        GUILayout.BeginVertical("box");
+        SetTempChildrenToZero(transform);
+        JUGAR();
+    }
 
-        GUILayout.Label("SELF");
-        PrintTransform(transform);
+    void SetTempChildrenToZero(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            if (child.name.Contains("[TEMP Layer]"))
+            {
+                child.localPosition = Vector3.zero;
+            }
 
+            SetTempChildrenToZero(child);
+        }
+    }
+
+    void JUGAR()
+    {
         if (workSpace != null)
         {
-            GUILayout.Space(10);
-            GUILayout.Label("[Work Space]");
-            PrintTransform(workSpace);
+            workSpace.localPosition = Vector3.zero;
+            workSpace.localRotation = Quaternion.Euler(90f, 0f, 0f);
         }
 
         if (workLayer != null)
         {
-            GUILayout.Space(10);
-            GUILayout.Label("Work Layer");
-            PrintTransform(workLayer);
+            workLayer.localPosition = startLocalPos;
+            workLayer.localRotation = startLocalRot;
         }
+    }
 
-        GUILayout.EndVertical();
+    void OnGUI()
+    {
+        // #if UNITY_EDITOR
+        // GUILayout.BeginVertical("box");
+        //
+        // GUILayout.Label("SELF");
+        // PrintTransform(transform);
+        //
+        // if (workSpace != null)
+        // {
+        //     GUILayout.Space(10);
+        //     GUILayout.Label("[Work Space]");
+        //     PrintTransform(workSpace);
+        // }
+        //
+        // if (workLayer != null)
+        // {
+        //     GUILayout.Space(10);
+        //     GUILayout.Label("Work Layer");
+        //     PrintTransform(workLayer);
+        // }
+        //
+        // GUILayout.EndVertical();
+        // #endif
     }
 
     void PrintTransform(Transform t)
     {
-        Vector3 pos = t.position;
-        Vector3 rot = t.eulerAngles;
+        Vector3 pos = t.localPosition;
+        Vector3 rot = t.localEulerAngles;
 
         GUILayout.Label($"Pos: {pos.x:F3}, {pos.y:F3}, {pos.z:F3}");
         GUILayout.Label($"Rot: {rot.x:F3}, {rot.y:F3}, {rot.z:F3}");
